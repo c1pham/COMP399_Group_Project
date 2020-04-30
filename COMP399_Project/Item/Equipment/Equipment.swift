dd295c6eb1a9456d0f03b286c7bfedf8352f0281
Christopher Pham <c1pham@student.bridgew.edu>
2020-04-30 00:50:36 -0400

    finish item hierarchy, although use implementation is needed to be finished which will be done once player class is created

--- COMP399_Project/Item/Equipment/Equipment.swift
+++ COMP399_Project/Item/Equipment/Equipment.swift
@@ -9,14 +9,48 @@
 import Foundation
 
 class Equipment : Item {
+    var grade : String
+    var statChanges : [String: Int]
     
-    override init(name: String, price: Int, description: String) {
+    override init(name: String, price: Int, description: String, rarity: String, statChanges : [String: Int]) {
+        self.grade = rarity
+        self.statChanges = statChanges
         super.init(name: name, price: price, description: description)
     }
     
     override func use(player: Any) {
+        // can make generic method that goes through all stat changes and applies them
         print("Equipment is equipped")
     }
     
+    override func display() {
+        return super.display() + ", Grade: \(self.grade)"
+    }
 }
+
+class Helmet : Equipment {
+    override init(name: String, price: Int, description: String, rarity: String, statChanges : [String: Int]) {
+        super.init(name: name, price: price, description: description, rarity: rarity, statChanges: statChanges)
+    }
+}
+
+class ChestPiece : Equipment {
+    override init(name: String, price: Int, description: String, rarity: String, statChanges : [String: Int]) {
+        super.init(name: name, price: price, description: description, rarity: rarity, statChanges: statChanges)
+    }
+}
+
+class Boot : Equipment {
+    override init(name: String, price: Int, description: String, rarity: String, statChanges : [String: Int]) {
+        super.init(name: name, price: price, description: description, rarity: rarity, statChanges: statChanges)
+    }
+}
+
+class Gloves : Equipment {
+    override init(name: String, price: Int, description: String, rarity: String, statChanges : [String: Int]) {
+        super.init(name: name, price: price, description: description, rarity: rarity, statChanges: statChanges)
+    }
+}
+
+
 
--- COMP399_Project/Item/Potion/Potion.swift
+++ COMP399_Project/Item/Potion/Potion.swift
@@ -9,14 +9,63 @@
 import Foundation
 
 
+
 class Potion : Item {
+    var stat: String
+    var amount: Int
     
-    override init(name: String, price: Int, description: String) {
+    init(name: String, price: Int, description: String, stat: String, amount: Int) {
+        self.stat = stat
+        self.amount = amount
         super.init(name: name, price: price, description: description)
     }
     
     override func use(player: Any) {
         print("Potion is used")
     }
+    
+    override func display() {
+        return super.display() + ", Stat: \(self.stat), Amount: \(self.amount)"
+    }
 }
 
+
+class HealPotion: Potion {
+    init(name: String, price: Int, description: String, amount : Int) {
+        super.init(name: name, price: price, description: description, stat: "health" ,amount: amount)
+    }
+    
+    override func use(player: Any) {
+        print("Healed")
+    }
+}
+
+
+class PernamentPotion: Potion {
+    
+    override init(name: String, price: Int, description: String, stat: String, amount : Int) {
+        super.init(name: name, price: price, description: description, stat: stat ,amount: amount)
+    }
+    
+    override func use(player: Any) {
+        print("pernament change")
+    }
+}
+
+class TemporaryPotion: Potion {
+    var duration : Int
+    
+    init(name: String, price: Int, description: String, stat: String, amount : Int, turns: Int) {
+        self.duration = turns
+        super.init(name: name, price: price, description: description, stat: stat, amount: amount)
+    }
+    
+    override func use(player: Any) {
+        print("temp change")
+    }
+    
+    override func display() -> String {
+        return super.display() + ", Amount: \(self.amount), Duration: \(duration)"
+    }
+    
+}
