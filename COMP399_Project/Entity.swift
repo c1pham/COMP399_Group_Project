ef01f2fe285e2aaa088c89d700c105aa02f6d182
Christopher Pham <c1pham@student.bridgew.edu>
2020-05-01 17:20:06 -0400

    added character class

--- COMP399_Project/Entity.swift
+++ COMP399_Project/Entity.swift
@@ -8,8 +8,222 @@
 
 import Foundation
 
-class Entity {
-    func toString() {
-        
+
+class Character {
+    
+    var name: String            //Name of Character
+    var curHp: Int              //Current Health value of character
+    var maxHp: Int              //Max Health value of character
+    var level = 1               //Level of Character
+    var stats: [Int]            //Stats: [Hp, Att, Def, Lck]
+    var sprite: String          //Sprite is a string since I don't know how to add a UIImage to this class
+    
+    //Will be used when the character is created
+    init (name: String, stats: [Int], sprite: String){
+        self.name = name
+        self.stats = stats
+        self.sprite = sprite
+        self.maxHp = stats[0] * 10
+        self.curHp = self.maxHp
     }
+    
+    //Used for enemies that are created
+    init(name: String, stats: [Int], sprite: String, level: Int) {
+        self.name = name
+        self.stats = stats
+        self.sprite = sprite
+        self.level = level
+        self.maxHp = stats[0] * 10              //Kind of useless for the enemies
+        self.curHp = self.maxHp
+    }
+    
+    //Basic Getters for the character
+    func getName() -> String{
+        return name
+    }
+    
+    func getSprite() -> String{
+        return sprite
+    }
+    
+    func getHealth() -> Int{
+        return stats[0]
+    }
+    
+    func getAttack() -> Int{
+        let critChance = arc4random_uniform(100)
+        if critChance <= stats[3]{
+            return stats[1] * 2
+        }
+        return stats[1]
+    }
+    
+    func getDefense() -> Int{
+        return stats[2]
+    }
+    
+    func getLuck() -> Int{
+        return stats[3]
+    }
+    
+    func getCurrentHealth() -> Int{
+        return curHp
+    }
+    
+    //If attacked, num is positive, if healed num is negative
+    func changeHealth(_ num: Int){
+        curHp -= num
+        if curHp > maxHp{
+            curHp = maxHp
+        }
+    }
+    
+}
+
+//Player has different circumstances than an enemy
+class Player: Character{
+
+    var curExp = 0              //Current number of experience the player has
+    var maxExp = 100            //Max number of exprience the player can have before level up
+    var statPoints = 0          //Points available after level up
+    var weaponEquip = 0         //No Weapon Equipped
+    var armorEquip = [0,0,0,0]  //No Armor Equipped
+    
+    //When Player levels up
+    func levelUp(){
+        
+        //Make sure that the current Exp is reduced to appropriate amount
+        curExp = curExp - maxExp
+        
+        //Make Max Experience earned more
+        maxExp = maxExp + level*level
+        
+        //Increase number of stats point by 5
+        statPoints += 5
+        
+        //Increase level by one
+        level += 1
+    }
+    
+    //Increase the various stats
+    func increaseHealth(){
+        stats[0] += 1
+        
+        //When you increase the health stat, make sure to update the max Hp to new value
+        maxHp = stats[0] * 10
+        curHp = maxHp
+    }
+    
+    func increaseAttack(){
+        stats[1] += 1
+    }
+
+    func increaseDefense(){
+        stats[2] += 1
+    }
+    
+    func increaseLuck(){
+        stats[3] += 1
+    }
+    
+    //Change attack by including the weapon
+    override func getAttack() -> Int {
+        let critChance = arc4random_uniform(100)
+        if critChance <= stats[3]{
+            return (stats[1] + weaponEquip) * 2
+        }
+        return stats[1] + weaponEquip
+    }
+    
+    //Change Defence by including the armor
+    override func getDefense() -> Int {
+        var totalDef = 0
+        
+        for i in armorEquip{
+            totalDef += i
+        }
+        return stats[2] + totalDef
+    }
+}
+
+//If false, player is alive, else player is dead
+func battleSequence(player: Character, enemy: Character) -> Bool{
+    
+    //While player is alive
+    while player.getCurrentHealth() > 0{
+        
+        //Player attacks first
+        enemy.changeHealth(player.getAttack())
+        
+        //If enemy dies, player is alive and dosen't take anymore damage
+        if enemy.getCurrentHealth() <= 0{
+            return false
+        }
+        
+        //Enemy attack next
+        player.changeHealth(enemy.getAttack())
+    }
+    
+    //If player is not alive, return true
+    return true
+}
+
+//Spawn some enemy at a certain level
+func spawnEnemy(level: Int) -> Character{
+    let name = ["Slime", "Goblin", "Skeleton"]              //List of enemy names
+    let modifier = ["Common", "Uncommon", "Rare"]           //List of modifier names
+    let stats = [[4,1,1,1],[3,2,1,2],[6,5,5,5]]             //Initial Stats of enemies
+    let sprite = ["temp0","temp1","temp2"]                  //This needs to be changed in order for the sprite to be created
+    let spawnChance = Int(arc4random_uniform(100))          //Chance that an enemy spawns
+    let modifierChance = Int(arc4random_uniform(100))       //Chance that an enemy spawns with a specific modifer
+    var enemyName: String                                   //Final name of enemy
+    var enemyStats: [Int]                                   //Final stats of enemy
+    var enemySprite: String                                 //Final sprite of enemy
+    
+    //Modifier 10% Rare, 30% Uncommon, 60% Common
+    if modifierChance < 100 && modifierChance >= 90{
+        enemyName = modifier[2]
+        enemyStats = [5,5,5,5]
+    } else if modifierChance < 90 && modifierChance >= 60 {
+        enemyName = modifier[1]
+        enemyStats = [2,2,2,2]
+    } else {
+        enemyName = modifier[0]
+        enemyStats = [0,0,0,0]
+    }
+    
+    //20% Chance for a Skeleton to Spawn
+    if spawnChance < 100 && spawnChance >= 80 {
+        enemyName += " " + name[2]
+        for i in 0..<4{
+            enemyStats[i] += stats[2][i]
+        }
+        enemySprite = sprite[2]
+        
+    //30% Chance for a Goblin to spawn
+    } else if spawnChance < 80 && spawnChance >= 50{
+        enemyName += " " + name[1]
+        for i in 0..<4{
+            enemyStats[i] += stats[1][i]
+        }
+        enemySprite = sprite[1]
+        
+    //50% Chance for a Slime to spawn
+    } else {
+       enemyName += " " + name[0]
+        for i in 0..<4{
+            enemyStats[i] += stats[0][i]
+        }
+        enemySprite = sprite[0]
+    }
+    
+    //Change stats according to enemy level
+    for i in 0..<enemyStats.count{
+        let statGained = Int(arc4random_uniform(UInt32(level)))
+        enemyStats[i] += statGained
+    }
+    
+    
+    //Enemy is created
+    return Character(name: enemyName, stats: enemyStats, sprite: enemySprite, level: level)
 }
--- COMP399_Project/ItemFactory.swift
+++ COMP399_Project/ItemFactory.swift
@@ -7,16 +7,194 @@
 //
 
 import Foundation
-
+/*
 class ItemFactory {
     
+    // we use enums to keep track of indexes of stats in the item arrays
     
     
     
+    /*
+    weak enum HEAL_POTION {
+        case NAME
+        case PRICE
+        case DESCRIPTION
+        case STAT
+        case AMOUNT
+    }*/
+    
+    let healPotionStats = [
+        ["Small Hp Potion", 0 , "Heals a little", "health", 10],
+        ["Small Hp Potion", 0 , "Heals a good amount", "health", 50],
+        ["Small Hp Potion", 0 , "Heals a lot", "health", 10]
+    ]
+    
+    /*
+    weak enum PERM_POTION {
+        case NAME
+        case PRICE
+        case DESCRIPTION
+        case STAT
+        case AMOUNT
+    }*/
+    
+    let permPotionStats = [
+        ["Small HP Boost Potion", 0 , "Pernamently Increase HP", "health", 10],
+        ["Small Attack Boost Potion", 0 , "Pernamently Increase HP", "attack", 10],
+        ["Small Defense Boost Potion", 0 , "Pernamently Increase HP", "defense", 10],
+        ["Small Luck HP Boost Potion", 0 , "Pernamently Increase HP", "luck", 10],
+        ["Medium HP Boost Potion", 0 , "Pernamently Increase HP", "health", 30],
+        ["Medium Attack Boost Potion", 0 , "Pernamently Increase HP", "attack", 30],
+        ["Medium Defense Boost Potion", 0 , "Pernamently Increase HP", "defense", 30],
+        ["Medium Luck HP Boost Potion", 0 , "Pernamently Increase HP", "luck", 30],
+        ["Large HP Boost Potion", 0 , "Pernamently Increase HP", "health", 100],
+        ["Large Attack Boost Potion", 0 , "Pernamently Increase HP", "attack", 100],
+        ["Large Defense Boost Potion", 0 , "Pernamently Increase HP", "defense", 100],
+        ["Large Luck HP Boost Potion", 0 , "Pernamently Increase HP", "luck", 100]
+    ]
+    /*
+    weak enum TEMP_POTION {
+        case NAME
+        case PRICE
+        case DESCRIPTION
+        case STAT
+        case AMOUNT
+        case DURATION
+    }*/
+    
+    let tempPotionStats = [
+        ["Small Temporary HP Boost Potion", 0 , "Pernamently Increase HP", "health", 10],
+        ["Small Temporary Attack Boost Potion", 0 , "Pernamently Increase HP", "attack", 10],
+        ["Small Temporary Defense Boost Potion", 0 , "Pernamently Increase HP", "defense", 10],
+        ["Small Temporary Luck HP Boost Potion", 0 , "Pernamently Increase HP", "luck", 10],
+        ["Medium Temporary HP Boost Potion", 0 , "Pernamently Increase HP", "health", 30],
+        ["Medium Temporary Attack Boost Potion", 0 , "Pernamently Increase HP", "attack", 30],
+        ["Medium Temporary Defense Boost Potion", 0 , "Pernamently Increase HP", "defense", 30],
+        ["Medium Temporary Luck HP Boost Potion", 0 , "Pernamently Increase HP", "luck", 30],
+        ["Large Temporary HP Boost Potion", 0 , "Pernamently Increase HP", "health", 100],
+        ["Large Temporary Attack Boost Potion", 0 , "Pernamently Increase HP", "attack", 100],
+        ["Large Temporary Defense Boost Potion", 0 , "Pernamently Increase HP", "defense", 100],
+        ["Large Temporary Luck HP Boost Potion", 0 , "Pernamently Increase HP", "luck", 100]
+    ]
+    
+    weak enum SWORD {
+        case NAME
+        case PRICE
+        case DESCRIPTION
+        case GRADE
+        case ATTACK
+        case LUCK
+    }
+    
+    let swordStats = [
+        ["Stick", 0, "A old stick", "scrap", 1 , 1],
+        ["Sharpen Bat", 0, "A old sword", "uncommon", 4, 4],
+        ["Rusty Knight's Sword", 0, "A knight's sword", "rare", 10 , 10],
+        ["Lighting Blade", 0, "A blade of lighting", "mythic", 30 , 30],
+        ["Excalibur", 0, "The King's choice of weaponry", "legendary", 100 , 100]
+        
+    ]
+    
+    weak enum CHEST_PIECE {
+        case NAME
+        case PRICE
+        case DESCRIPTION
+        case GRADE
+        case HEALTH
+        case DEFENSE
+    }
+    
+    let chestPieceStats = [
+        ["Leather Coat", 0, "A old leather coat", "scrap", 1 , 1],
+        ["Chain Mail", 0, "A old rusty chain mail", "uncommon", 4, 4],
+        ["Rusty Knight's Chestpiece", 0, "A knight's chestpiece", "rare", 10 , 10],
+        ["Golden Fleece", 0, "A golden fleece", "mythic", 30 , 30],
+        ["Author's chestpiece", 0, "The King's choice for an chestpieces", "legendary", 100 , 100]
+    ]
+    
+    weak enum HELEMET {
+        case NAME
+        case PRICE
+        case DESCRIPTION
+        case GRADE
+        case HEALTH
+        case DEFENSE
+    }
+    
+    let helmetStats = [
+        ["Bowl", 0, "A broken bowl", "scrap", 1 , 1],
+        ["Miners Helmet", 0, "A old miner's helmet", "uncommon", 4, 4],
+        ["Rusty Knight's Helmet", 0, "A knight's helmet", "rare", 10 , 10],
+        ["Barbarian King Helmet", 0, "A helmet from the last known Barbarian King", "mythic", 30 , 30],
+        ["Author's helmet", 0, "The King's choice for an helmet", "legendary", 100 , 100]
+    ]
+    
+    weak enum BOOT {
+        case NAME
+        case PRICE
+        case DESCRIPTION
+        case GRADE
+        case HEALTH
+        case DEFENSE
+    }
+    
+    let bootStats = [
+        ["Ripped Socks", 0, "A broken bowl", "scrap", 1 , 1],
+        ["Miners Boots", 0, "A old miner's boots", "uncommon", 4, 4],
+        ["Rusty Knight's Boots", 0, "A knight's boots", "rare", 10 , 10],
+        ["Flash's shoes", 0, "A old shoe from the flash", "mythic", 30 , 30],
+        ["Author's boots", 0, "The King's choice for an boots", "legendary", 100 , 100]
+    ]
+    
+    weak enum GLOVES {
+        case NAME
+        case PRICE
+        case DESCRIPTION
+        case GRADE
+        case HEALTH
+        case DEFENSE
+        case LUCK
+    }
+    
+    let glovesStats = [
+        ["Ripped mittins", 0, "A broken bowl", "scrap", 1 , 1],
+        ["Miners gloves", 0, "A old miner's boots", "uncommon", 4, 4],
+        ["Rusty Knight's Gauntlet", 0, "A knight's helemt", "rare", 10 , 10],
+        ["Kan's Gaunlets", 0, "The gauntlet the legend Kan had", "mythic", 30 , 30],
+        ["Author's Gauntlets", 0, "The King's choice for an boots", "legendary", 100 , 100]
+    ]
     
     
     
-    func produce(name : String) -> Item? {
-        return nil
+    func produceItem(name : String, type: String) -> Item? {
+        var item : Item? = nil
+        if (type.lowercased() == "healpotion") {
+            item = HealPotion(name: String(healPotionStats[HEAL_POTION.NAME]), price: Int(healPotionStats[HEAL_POTION.PRICE]), description: String(healPotionStats[HEAL_POTION.DESCRIPTION]), amount: Int(healPotionStats[HEAL_POTION.AMOUNT]))
+        } else if (type.lowercased() == "temppotion") {
+            item = TemporaryPotion(name: String(tempPotionStats[TEMP_POTION.NAME]), price: Int(tempPotionStats[TEMP_POTION.PRICE]), description: String(tempPotionStats[TEMP_POTION.DESCRIPTION]), stat: String(tempPotionStats[TEMP_POTION.STAT]), amount: Int(tempPotionStats[TEMP_POTION.AMOUNT]), turns: Int(tempPotionStats[TEMP_POTION.DURATION]))
+            
+        } else if (type.lowercased() == "permpotion") {
+            
+        }
+        return item
+    }
+    
+    func findIndex(itemName: String, itemStats: [[Any]]) -> Int {
+        for num in 0 ... itemStats.count {
+            if (itemStats[num][0] as! String == itemName) {
+                return num
+            }
+        }
+        return -1
     }
 }
+
+
+
+
+var factory = ItemFactory()
+
+factory.findIndex(itemName : "Medium Hp Potion", itemStats: factory.healPotionStats)
+
+
+*/
